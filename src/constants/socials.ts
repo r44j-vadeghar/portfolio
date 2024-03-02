6bfa18da506abe4bf57028155c5a8089bc062147
Raj Vadeghar <instigator0002@gmail.com>
2024-03-02 17:16:05 +0530

    refactor: update social links, fix(css): mobile responsiveness

---
+++ src/constants/socials.ts
@@ -0,0 +1,33 @@
+import GithubSvgReference from "@/assets/socials/github.svg";
+import InstagramSvgReference from "@/assets/socials/instagram.svg";
+import LinkedinSvgReference from "@/assets/socials/linkedin.svg";
+import MailSvgReference from "@/assets/socials/mail.svg";
+import TwitterSvgReference from "@/assets/socials/twitter.svg";
+
+export default {
+  mail: {
+    url: "mailto:vadegharraj@gmail.com",
+    title: "Raj Vadeghar Gmail",
+    icon: MailSvgReference.src
+  },
+  github: {
+    url: "https://github.com/r44j-vadeghar",
+    title: "Raj Vadeghar Github",
+    icon: GithubSvgReference.src
+  },
+  linkedin: {
+    url: "https://www.linkedin.com/in/r44j/",
+    title: "Raj Vadeghar LinkedIn",
+    icon: LinkedinSvgReference.src
+  },
+  instagram: {
+    url: "https://www.instagram.com/r44j.vadeghar/",
+    title: "Raj Vadeghar Instagram",
+    icon: InstagramSvgReference.src
+  },
+  twitter: {
+    url: "https://twitter.com/VadegharRaj",
+    title: "Raj Vadeghar Twitter",
+    icon: TwitterSvgReference.src
+  }
+};
